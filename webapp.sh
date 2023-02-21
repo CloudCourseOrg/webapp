196aefbc35842f9b1e363b6d5e37227d370a7753
Arpit Jain <jain.arpit@northeastern.edu>
2023-02-21 16:10:27 -0500

    service

--- webapp.sh
+++ webapp.sh
@@ -3,7 +3,7 @@
 sudo yum update -y
 
 sudo yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
 
 sudo yum install mariadb mariadb-server -y
@@ -12,7 +12,28 @@ sudo mysqladmin -u root password "password"
 mysqladmin -u root --password=password --host=localhost --port=3306 create cloud_web
 sudo systemctl enable mariadb
 
-unzip webapp.zip
-cd webapp
+cd /home/ec2-user/webapp
 npm i
-DBHOST=localhost DBUSER=root DBPASS=password DATABASE=cloud_web PORT=3000 npm start
+
+#Giving exec writes to owner, user and group
+chmod -R 755 node_modules/
+rm -rf node_modules/
+npm i
+
+sudo cp packer/webapp.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
+
+
+
+# Install nginx
+sudo amazon-linux-extras list | grep nginx
+sudo amazon-linux-extras enable nginx1
+sudo yum clean metadata
+sudo yum -y install nginx
+sudo systemctl enable nginx
+sudo cp packer/nginx.conf /etc/nginx/
+sudo systemctl restart nginx
+sudo systemctl reload nginx
