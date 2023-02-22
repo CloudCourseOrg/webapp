38c4beb4e2545c0ddea895e278ea9d9f62bda8e5
Arpit Jain <jain.arpit@northeastern.edu>
2023-02-22 02:05:41 -0500

    databse test3

--- webapp.sh
+++ webapp.sh
@@ -8,11 +8,17 @@ sudo yum install -y nodejs
 
 sudo yum install mariadb mariadb-server -y
 
-export DATABASEUSER=${mysql_USER}
-export DATABASEPASSWORD=${mysql_PASSWORD}
-export DATABASE=${mysql_DB}
-export PORT=${mysql_PORT}
-export DATABASEHOST=${mysql_HOST}
+# export DATABASEUSER=${mysql_USER}
+# export DATABASEPASSWORD=${mysql_PASSWORD}
+# export DATABASE=${mysql_DB}
+# export PORT=${mysql_PORT}
+# export DATABASEHOST=${mysql_HOST}
+
+# export DATABASEUSER=${mysql_USER}
+# export DATABASEPASSWORD=${mysql_PASSWORD}
+# export DATABASE=${mysql_DB}
+# export PORT=${mysql_PORT}
+# export DATABASEHOST=${mysql_HOST}
 
 sudo systemctl start mariadb
 sudo mysqladmin -u ${mysql_USER} password ${mysql_PASSWORD}
