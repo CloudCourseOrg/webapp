6227694fede7257450bd319b77520e676668a351
arpitjain1122 <113169107+arpitjain1122@users.noreply.github.com>
2023-02-24 14:17:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = require('./server');
 
 //app.listen(process.env.PORT);
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3100;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
 app.listen(port, () => console.log('Server listening on port ' + port));
 
 module.exports = app;
