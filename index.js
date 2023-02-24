b459c307f5b2b1c004fa4366673a2957400ee171
arpitjain1122 <113169107+arpitjain1122@users.noreply.github.com>
2023-02-24 15:35:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-//const app = require("./server")
-//const dotenv = require("dotenv")
-//dotenv.config()
-
-//app.listen(process.env.PORT, () => {
-  //console.log("server is running ")
-//})
-
-
 const app = require('./server');
-
-//app.listen(process.env.PORT);
-
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
-app.listen(port, () => console.log('Server listening on port ' + port));
-
-module.exports = app;
+const dotenv = require('dotenv');
+dotenv.config();
+//
+app.listen(process.env.PORT);
