1b84659d51de28f9891e7694c9447348de2b0ad1
arpitjain1122 <113169107+arpitjain1122@users.noreply.github.com>
2023-02-24 15:34:17 -0500

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,54 +1,47 @@
-const { DataTypes } = require("sequelize")
-
-/**
- * Creating user model using sequelize
- */
+const { DataTypes } = require("sequelize");
 
 const createUserModel = (sequelize) => {
-  let User = sequelize.define(
-    "user",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      account_created: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      account_updated: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
+    let User = sequelize.define("user", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        account_created: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        account_updated: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
     },
     {
-      updatedAt: "account_updated",
-      createdAt: "account_created",
+        updatedAt: 'account_updated',
+        createdAt: 'account_created',
     },
     {
-      initialAutoIncrement: 1,
-    }
-  )
+        initialAutoIncrement: 1,
+    });
 
-  return User
+    return User;
 }
 
-module.exports = createUserModel
+module.exports = createUserModel;
