6c1e10dae49428dca2de3daf4bc465e957c13d61
Arpit Jain <jain.arpit@northeastern.edu>
2023-03-22 00:31:57 -0400

    test6

--- config/logger.js
+++ config/logger.js
@@ -52,7 +52,7 @@ var winston = require('winston');
 
 const logger = winston.createLogger({
   format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: '../../../../var/log/combined.log'})],
+  transports: [new winston.transports.File({ filename: '/var/log/combined.log'})],
 });
 
 module.exports = logger;
