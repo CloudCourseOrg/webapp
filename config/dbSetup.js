85b1287af5ae957ddea8cee43e05c2d02452d558
Arpit Jain <jain.arpit@northeastern.edu>
2023-03-29 19:40:28 -0400

    arpittest1

--- config/dbSetup.js
+++ config/dbSetup.js
@@ -1,26 +1,29 @@
 const Sequelize = require("sequelize");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const createUserModel = require('../models/user.model');
-const createProductModel = require('../models/product.model');
-const createImageModel = require('../models/image.model');
+const createUserModel = require("../models/user.model");
+const createProductModel = require("../models/product.model");
+const createImageModel = require("../models/image.model");
 
-console.log(    process.env.DATABASE,
-    process.env.DBUSER,
-    process.env.DBPASS, process.env.DBHOST)
-    
+console.log(
+  process.env.DATABASE,
+  process.env.DBUSER,
+  process.env.DBPASS,
+  process.env.DBHOST
+);
+
 const sequelize = new Sequelize(
-    process.env.DATABASE,
-   process.env.DBUSER,
-   process.env.DBPASS,
-    {
-      host: process.env.DBHOST,
-      dialect: 'mysql',
-      "define": {
-            freezeTableName: true
-        }     
-    }
+  process.env.DATABASE,
+  process.env.DBUSER,
+  process.env.DBPASS,
+  {
+    host: process.env.DBHOST,
+    dialect: "mysql",
+    define: {
+      freezeTableName: true,
+    },
+  }
 );
 
 let db = {};
